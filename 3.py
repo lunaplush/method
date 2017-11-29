335a42fbd6241afdd63aca436e433c9252ab69c3
lunaplush <lunaplus@mail.ru>
2017-11-29 10:03:56 +0300

    final graph

--- 3.py
+++ 3.py
@@ -40,7 +40,7 @@ _ = calculate_data(auto1)
 _ = calculate_data(auto2)
 #%%
 #plt.style.use("seaborn-pastel")
-plt.style.use("grayscale")
+#plt.style.use("grayscale")
 
 
 fig, ax = plt.subplots()
@@ -64,6 +64,13 @@ ax.set_yticklabels(['{:3.0f}%'.format(x) for x in y_vals])
 
 x_vals = ax.get_xticks()
 ax.set_xticklabels(['{:3.0f}%'.format(x) for x in x_vals])
+ax.tick_params(axis = 'both', bottom = "on")
+ax.grid(False)
+ax.spines["left"].set_visible(True)
+ax.spines["bottom"].set_visible(True)
+ax.axhline()
+ax.axvline()
+
 plt.xlabel("Относительная цена автомобиля")
 plt.ylabel("Доля автомобилей с данной ценой")
 #ax.set_xticklabels(['' for x in vals[1:-3]])
@@ -71,11 +78,11 @@ plt.ylabel("Доля автомобилей с данной ценой")
 
 ax2 = mpl_il.inset_axes(plt.gca(), width='80%', height='80%', loc=1, borderpad = 1)
 
-ax2.plot(auto2.cena_relative, auto2.num_relative, marker = "o", color="k" )
-ax2.plot(auto1.cena_relative, auto1.num_relative, marker = "*", color="k" , ms =8)
+ax2.plot(auto2.cena_relative, auto2.num_relative, marker = "o", color="k", label ="15 когорт" )
+ax2.plot(auto1.cena_relative, auto1.num_relative, marker = "*", color="k" , ms =8, label = "24 когорты")
 
 
-ax2.set_xlim(0,2)
+ax2.set_xlim(0,2.02)
 ax2.set_ylim(0,40)
 ax2.grid(b="on",axis ="both", linestyle='dashed', linewidth = 1, color = "grey")
 
@@ -88,12 +95,11 @@ ax2.set_yticklabels(['{:3.0f}%'.format(x) for x in y_vals])
 
 x_vals = ax2.get_xticks()
 ax2.set_xticklabels(['{:3.1f}%'.format(x) for x in x_vals])
-
+ax2.legend()
 #ax2.set_xticks([0]+auto1.cena_relative_right)
 #ax2.margins(x=0.5)
 
 #mark_inset(ax,ax2,loc1 = 2, loc2 = 3, fc = "none", linestyle = ":")
-ax.grid(b="off",axis ="both")
 #ax.semilogx()
 #plt.xscale("log")
 
@@ -105,3 +111,14 @@ plt.savefig("5.png")
 #%%
 #plt.figure()
 #plt.bar(np.arange(10), height = np.arange(10))
+
+#tty = np.random.rand(10)
+#ttx =  np.linspace(0,10,10)
+#fig1, ax1 = plt.subplots()
+#plt.plot(ttx,tty)
+#fig1.set_facecolor("w")
+#fig1.set_frameon(False)
+#ax1.set_axis_on()
+#ax1.grid(False)
+#ax1.spines["left"].set_position('center')
+#ax1.spines["left"].set_visible(True)
